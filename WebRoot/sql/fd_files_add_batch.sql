ee6e2dc2a094da9ba9ec5e26eb18802b188a6070
qwl <qwl@ncmem.com>
2016-09-22 17:57:50 +0800

    更新批量生成文件ID存储过程 更新批量生成文件ID代码 更新批量检查MD5代码

--- WebRoot/sql/fd_files_add_batch.sql
+++ WebRoot/sql/fd_files_add_batch.sql
@@ -1,6 +1,6 @@
 USE [HttpUploader6]
 GO
-/****** 对象:  StoredProcedure [dbo].[fd_fileProcess]    脚本日期: 04/03/2016 17:48:05 ******/
+/****** 对象:  StoredProcedure [dbo].[fd_add_batch]    脚本日期: 07/28/2016 17:42:12 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -8,30 +8,30 @@ GO
 
 -- =============================================
 -- Author:		zysoft
--- Create date: 2016-07-31
--- Description:	批量添加子文件
+-- Create date: 2016-08-04
+-- 更新 2016-09-06 使用临时表解决ID数量过多的问题。
+-- Description:	批量分配文件夹ID和文件ID，提供给上传文件夹使用，在初始化时使用
 -- =============================================
-ALTER PROCEDURE [dbo].[fd_files_add_batch]	
-	 @f_count int			--文件总数，要单独增加一个文件夹
-	,@fd_count int			--文件夹总数
-	,@f_ids varchar(8000) output
-	,@fd_ids varchar(8000) output
+CREATE PROCEDURE [dbo].[fd_files_add_batch]	
+	 @f_count int	--文件总数，要单独增加一个文件夹
+	,@fd_count int	--文件夹总数
 AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
+	
+	--使用临时表存储ID
+	create table #tb_ids(t_file bit,t_id int)
 
     DECLARE @i int;
 	set @i = 0;
-	set @f_ids = '';
-	set @fd_ids = '';
 
 	/*批量添加文件夹*/
 	while @i < @fd_count
 	begin
 		insert into up6_folders(fd_pid) values(0);
-		set @fd_ids = @fd_ids + ltrim(str(@@IDENTITY)) + ',';
+		insert into #tb_ids values(0,@@IDENTITY)
 		set @i = @i + 1;
 	end
 
@@ -40,13 +40,10 @@ BEGIN
 	while @i < @f_count
 	begin
 		insert into up6_files(f_pid) values(0)
-		set @f_ids = @f_ids + ltrim(str(@@IDENTITY)) + ',';
+		insert into #tb_ids values(1,@@IDENTITY)
 		set @i = @i+1
 	end
 	
 	--清除,
-	set @f_ids = substring(@f_ids,1,len(@f_ids)-1);
-	set @fd_ids = substring(@fd_ids,1,len(@fd_ids) -1);	
+	select * from #tb_ids;
 END
-
-
--- WebRoot/sql/fd_files_check.sql
+++ WebRoot/sql/fd_files_check.sql
@@ -8,39 +8,37 @@ GO
 
 -- =============================================
 -- Author:		zysoft
--- Create date: 2016-08-04
+-- 2016-08-04 创建
+-- 2016-09-06 更新，单个md5长度和md5字符串总长度由外部传入
 -- Description:	批量查询相同MD5的文件
 -- =============================================
 CREATE PROCEDURE [dbo].[fd_files_check]	
-	 @md5s varchar(8000)	--md5列表:a,b,c,d
+	 @md5s		varchar(8000)	--md5列表:a,b,c,d
+	,@md5_len	int--单个MD5长度
+	,@md5s_len	int --md5字符串总长度
 AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
-	--拆分md5	
-	declare @md5_len int--单个md5长度，固定值
-	declare @md5_item varchar(40)
-	declare @md5_len_total int --md5总长度
+	--拆分md5		
+	declare @md5_item varchar(40)	
 	declare @md5_cur int
 	declare @split_pos int--当前分割符位置	
 	DECLARE @t_md5 TABLE(md5 varchar(40))
 	
 	set @md5_cur = 0
 	set @split_pos = charindex(',',@md5s)
-	set @md5_len_total = len(@md5s)
-	set @md5_len = @split_pos
-	if @md5_len = 0 		
-		set @md5_len = @md5_len_total
-
+	
+	
 	--有多个md5
-	if @md5_len_total > @md5_len
+	if @md5s_len > @md5_len
 	begin
-		while @md5_cur < @md5_len_total
+		while @md5_cur < @md5s_len
 		begin
-			set @md5_item = substring(@md5s,@md5_cur+1,@md5_len-1)			
+			set @md5_item = substring(@md5s,@md5_cur+1,@md5_len)			
 			insert into @t_md5(md5) values(@md5_item)
-			set @md5_cur = @md5_cur + @md5_len
+			set @md5_cur = @md5_cur + @md5_len + 1
 		end
 	end
 	else--只有一个md5
